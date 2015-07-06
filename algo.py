ff4916e34596144e736664aad16b201abab57895
ayratzay <ayratzay@gmail.com>
2015-07-07 02:10:06 +0300

    linear regression(no matrix) linear regression(matrix, scalar normalization, learning rate graph, learing rate optimazation) normal equation for linear regressions

---
+++ algo.py
@@ -0,0 +1,115 @@
+__author__ = 'Freeman'
+
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def linear_regression(x0, x1, theta0, theta1):
+    return x0 * theta0 + x1 * theta1
+
+
+def squared_delta(h_x, y):
+    return (h_x - y) ** 2
+
+
+def mse(ds, t0, t1):
+    sqe = [squared_delta(linear_regression(x0, x1, t0, t1), y) for (x0, x1, y) in ds]
+    return sum(sqe) / (2 * len(sqe))
+
+
+def derivative_gradient(h_x, y, der_x):
+    return (h_x - y) * der_x
+
+
+def derivative_delta(ds, t0, t1, x_index):
+    gd = [derivative_gradient(linear_regression(x0, x1, t0, t1), y, (x0, x1)[x_index]) for (x0, x1, y) in ds]
+    return sum(gd) / len(gd)
+
+
+class LinerRegressionNoMatrix:
+    def __init__(self, learning_rate=0.05, iterations=1000):
+        self.learning_rate = learning_rate
+        self.theta = None
+        self.iterations = iterations
+        self.mse = None
+
+    def __repr__(self):
+        return ("linear regression model")
+
+    def fit(self, ds):
+        i = 0
+
+        # print (t0, t1, mse(dataset, t0, t1))
+        t0, t1 = random.randint(0, 10), random.randint(0, 10)
+        while i < self.iterations:
+            t0, t1 = t0 - self.learning_rate * derivative_delta(ds, t0, t1,
+                                                                0), t1 - self.learning_rate * derivative_delta(ds, t0,
+                                                                                                               t1, 1)
+            i += 1
+        self.theta = (t0, t1)
+        self.mse = mse(ds, t0, t1)
+
+    def predict(self, x):
+        return linear_regression(1, x, *self.theta)
+
+    def print_mse(self):
+        print ("MSE is " + str(self.mse))
+
+
+class LinearRegressionMatrix:
+    def __init__(self, learning_rate=0.05, iterations=1000):
+        self.learning_rate = learning_rate
+        self.iterations = iterations
+        self._theta = None
+        self._mse = None
+
+    def __repr__(self):
+        return "Linear regression based on matricies"
+
+    def _scalar_normalization(self, X):
+        mean_v = X.mean(axis=0)
+        len_v = X.max(axis = 0) - X.min(axis = 0)
+        mean_v[0], len_v[0] = 0, 1
+        X_new = (X - mean_v)/ len_v
+        return X_new
+
+    def fit(self, X, y):
+        global hist
+        X = self._scalar_normalization(X)
+        theta = np.random.randint(10, size=X.shape[1])
+        sample_size = X.shape[0]
+
+        mse_prev = None
+        for i in xrange(self.iterations):
+            error_array = np.dot(X, theta) - y
+            mse = sum(error_array ** 2 / (2 * sample_size))
+            if mse_prev:
+                if (mse_prev - mse) < 0.001:
+                    break
+            mse_prev = mse
+            theta = theta - theta * (self.learning_rate * np.dot(error_array.T, X) / sample_size)
+        hist[self.learning_rate] = (i, mse)
+        self._mse = mse
+        self._theta = theta
+
+    def predict(self, x):
+        return  sum(x * self._theta)
+
+
+class NormalEquation:
+
+    def __init__(self, ):
+        self._mse = None
+        self._theta = None
+
+    def __repr__(self):
+        return "Solving equation with normal equation"
+
+    def solve(self,X, y):
+        sq = np.dot(X.T, X)
+        inv = np.linalg.inv(sq)
+        step = np.dot(inv, X.T)
+        self._theta = np.dot(step, y)
+        error_array = np.dot(X, self._theta) - y
+        self._mse = sum(error_array ** 2 / (2 * X.shape[0]))
---
+++ main.py
@@ -0,0 +1,44 @@
+__author__ = 'Freeman'
+
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from algo import LinerRegressionNoMatrix
+from algo import LinearRegressionMatrix
+
+import timeit
+
+start_time = timeit.default_timer()
+
+for i in xrange(100):
+    dataset = [(1, a, a + random.gauss(0,1)) for a in range(100)] #(x, y)
+    m = LinerRegressionNoMatrix(learning_rate=0.05, iterations=10000)
+    m.fit(dataset)
+    m.predict(4)
+
+print(timeit.default_timer() - start_time)
+
+#dataset = [(1, a, a ) for a in range(10)] #(x, y)
+
+start_time = timeit.default_timer()
+
+hist = {}
+for i in [x/10000.0 for x in range(1,3001) if x % 5 == 0]:
+    mm = LinearRegressionMatrix(learning_rate=i, iterations=100)
+    dataset = [(1, a, a + random.gauss(0,1)) for a in range(10)] #(x, y)
+    dataset_matix = np.array(dataset)
+    X = dataset_matix[:, :2]
+    y = dataset_matix[:, 2]
+    mm.fit(X, y)
+#mm.predict(np.array([1, 4]))
+
+lr, it, mse = [],[],[]
+for key,value in hist.iteritems():
+    lr.append(key)
+    it.append(value[0])
+    mse.append(value[1])
+
+lr.sort(), mse.sort(), it.sort()
+
+plt.plot(lr, mse, 'b-')
+plt.plot(lr, it, 'r--')
